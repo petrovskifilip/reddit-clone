30b11a681e2241095cb038c2383eff4f118d57f7
Filip Petrovski <filip.petrovski.1@students.finki.ukim.mk>
2022-05-11 14:54:27 +0200

    Implement spring security.

---
+++ src/main/java/wp/redditclone/security/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package wp.redditclone.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import wp.redditclone.model.User;
+import wp.redditclone.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(email);
+        if(!user.isPresent()) {
+            throw new UsernameNotFoundException(email);
+        }
+        return user.get();
+
+    }
+}
